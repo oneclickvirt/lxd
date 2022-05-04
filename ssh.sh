0f9c10160dc46e2c286eac27b0f78928c77c1b7e
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-05-04 18:52:26 +0800

    Create ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,15 +1,40 @@
-#!/bin/csh
-read -p "password:" PASSWORD;
-eval "echo $PASSWORD > .env";
-sudo service iptables stop 2> /dev/null ; chkconfig iptables off 2> /dev/null ;
-sudo sed -i.bak '/^SELINUX=/cSELINUX=disabled' /etc/sysconfig/selinux;
-sudo sed -i.bak '/^SELINUX=/cSELINUX=disabled' /etc/selinux/config;
-sudo setenforce 0;
-echo root:"$PASSWORD" |sudo chpasswd root;
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo apt update -y;
-sudo apt install curl -y;
-sudo apt install sshpass;
-sudo apt-get install openssh-server -y;
-sudo service sshd restart;
+#!/bin/bash
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
+RELEASE=("debian" "ubuntu" "centos" "centos")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install")
+PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove")
+
+[[ $EUID -ne 0 ]] && exit 1
+
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && exit 1
+
+
+if [ $SYSTEM = "ubuntu" ]; then
+  eval "echo $SYSTEM > .env";
+  sudo service iptables stop 2> /dev/null ; chkconfig iptables off 2> /dev/null ;
+  sudo sed -i.bak '/^SELINUX=/cSELINUX=disabled' /etc/sysconfig/selinux;
+  sudo sed -i.bak '/^SELINUX=/cSELINUX=disabled' /etc/selinux/config;
+  sudo setenforce 0;
+  echo root:"$SYSTEM" |sudo chpasswd root;
+  sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+  sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+  sudo apt update -y;
+  sudo apt install curl -y;
+  sudo apt install sshpass;
+  sudo apt-get install openssh-server -y;
+  sudo service sshd restart;
+else
+  echo "error"
+fi
+
