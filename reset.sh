519abfc27a5934c002298c694ff1786fcf1acf4d
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-05-05 09:04:46 +0800

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -4,12 +4,8 @@ lxc exec "$1" -- sudo apt-get install dos2unix curl -y
 lxc exec "$1" -- curl -L https://raw.githubusercontent.com/spiritLHLS/lxc/main/ssh.sh -o ssh.sh
 lxc exec "$1" -- dos2unix ssh.sh
 lxc exec "$1" -- chmod +x ssh.sh
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
+sleep 6
 lxc exec "$1" -- sudo ./ssh.sh "$2"
 echo "$2"
+sleep 6
 rm -rf "$0"
