e1515ed239fc86439e4989820244bb0bf0c601c0
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-05-05 17:51:05 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,7 +7,7 @@ lxc config device set "$1" root limits.read 150Iops
 lxc config device set "$1" root limits.write 150Iops
 lxc config device set "$1" root limits.cpu.priority 0
 lxc config device set "$1" root limits.disk.priority 0
-lxc config device set "$1" root limits.network.priority 5
+lxc config device set "$1" root limits.network.priority 0
 lxc start "$1"
 lxc exec "$1" -- sudo apt-get install dos2unix curl -y
 lxc exec "$1" -- curl -L https://raw.githubusercontent.com/spiritLHLS/lxc/main/ssh.sh -o ssh.sh
