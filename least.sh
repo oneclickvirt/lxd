f047865e7568e5049472365b7437239d48a59abb
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-05-24 23:55:01 +0800

    Update least.sh

--- least.sh
+++ least.sh
@@ -19,7 +19,9 @@ lxc config set "$1" limits.memory.swap.priority 1
 for ((a=1;a<"$2";a++)); do
   lxc copy "$1" "$1"$a
   name="$1"$a
-  sshn=$(( 20000 + a ))
+  sshn=$(( 22222 + a ))
+  nat1=$(( 30000 + (a-1)*25 + 1))
+  nat2=$(( 30000 + a*25 ))
   ori=$(date | md5sum)
   passwd=${ori: 2: 9}
   lxc start "$1"$a
@@ -27,12 +29,12 @@ for ((a=1;a<"$2";a++)); do
   lxc exec "$1"$a -- apt update -y
   lxc exec "$1"$a -- sudo dpkg --configure -a
   lxc exec "$1"$a -- sudo apt-get update
-  lxc exec "$1"$a -- sudo apt-get install dos2unix curl wget -y
+  lxc exec "$1"$a -- sudo apt-get install dos2unix curl -y
   lxc exec "$1"$a -- curl -L https://raw.githubusercontent.com/spiritLHLS/lxc/main/ssh.sh -o ssh.sh
   lxc exec "$1"$a -- chmod 777 ssh.sh
   lxc exec "$1"$a -- dos2unix ssh.sh
   lxc exec "$1"$a -- sudo ./ssh.sh $passwd
-  lxc exec "$1"$a -- sudo apt-get autoremove
   lxc config device add "$1"$a ssh-port proxy listen=tcp:0.0.0.0:$sshn connect=tcp:127.0.0.1:22
-  echo "$name $sshn $passwd" >> log
+  lxc config device add "$1"$a nat-ports proxy listen=tcp:0.0.0.0:$nat1-$nat2 connect=tcp:127.0.0.1:$nat1-$nat2
+  echo "$name $sshn $passwd $nat1 $nat2" >> log
 done
