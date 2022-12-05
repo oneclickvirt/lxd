1080417ccfb02f708c4eabeb26c8113ad9c925b9
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-05 21:29:47 +0800

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#./backend.sh 内存大小以MB计算 硬盘大小以GB计算
-
+# 环境安装
 # 安装vnstat
 apt update
 apt install wget sudo curl -y
@@ -28,21 +27,3 @@ if [ $? -ne 0 ]; then
 fi
 ! chkconfig vnstat on && echo "replace chkconfig with sysv-rc-conf" && sysv-rc-conf vnstat on 
 service vnstat start
-
-# # 内存设置
-# apt install dos2unix ufw -y
-# curl -L https://raw.githubusercontent.com/spiritLHLS/lxc/main/swap2.sh -o swap2.sh && chmod +x swap2.sh
-# ./swap2.sh "$1"
-# # lxd安装，硬盘设置
-# apt -y install zfsutils || apt -y install zfs
-# apt install snapd -y
-# snap remove lxd -y >/dev/null 2>&1
-# ! snap install lxd -y && snap install core -y
-# snap install lxd -y
-# /snap/bin/lxd init --storage-backend zfs --storage-create-loop "$2" --storage-pool default --auto
-# ! lxc -h >/dev/null 2>&1 && echo 'alias lxc="/snap/bin/lxc"' >> /root/.bashrc && source /root/.bashrc
-# export PATH=$PATH:/snap/bin
-# ! lxc -h >/dev/null 2>&1 && echo 'Failed install lxc' && exit
-# # 设置镜像不更新
-# lxc config unset images.auto_update_interval
-# lxc config set images.auto_update_interval 0
