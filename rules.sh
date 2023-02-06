09355b8084afc2579e117ba670dafaecf741fc74
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-06 12:38:19 +0800

    Update rules.sh

--- rules.sh
+++ rules.sh
@@ -21,7 +21,7 @@ apparmor_parser -r /etc/apparmor.d/local/usr.bin.lxc-execute
 
 # 屏蔽流量
 iptables -F
-blocked_ports=(20 21 22 23 25 53 67 68 69 80 110 139 143 161 389 443 1433 1521 2094 3306 3389 5000 5432 5632 5900 6379 7001 8080 8888 9200 10000 27017 22122 54321 65432 )
+blocked_ports=( 20 21 22 23 25 53 67 68 69 80 110 139 143 161 389 443 1433 1521 2094 3306 3389 5000 5432 5632 5900 6379 7001 8080 8888 9200 10000 27017 22122 54321 65432 )
 for port in "${blocked_ports[@]}"; do
     iptables -A OUTPUT -m owner --uid-owner 100000-165536 -d 0.0.0.0/0 -p tcp --dport "$port" -j DROP
     iptables -A OUTPUT -m owner --uid-owner 100000-165536 -d 0.0.0.0/0 -p udp --dport "$port" -j DROP
