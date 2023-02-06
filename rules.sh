6cba44ba9f93970a140e3edb894a8f461510896b
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-06 12:19:07 +0800

    Update rules.sh

--- rules.sh
+++ rules.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # from https://github.com/spiritLHLS/lxc
 
+# 屏蔽安装包
 if ! dpkg -s apparmor &> /dev/null; then
   apt-get install apparmor
 fi
-
-# 获取所有容器的名字
 containers=$(lxc list -c n | awk '{print $2}')
-
-# 遍历每个容器
 for container_name in $containers
 do
   echo "lxc profile set $container_name restrictions" \
@@ -25,10 +22,12 @@ sudo bash -c 'cat > /etc/apparmor.d/local/usr.bin.lxc-execute << EOL
 EOL'
 apparmor_parser -r /etc/apparmor.d/local/usr.bin.lxc-execute
 
-
+# 屏蔽流量
 iptables -F
 blocked_ports=(22 21 54321 80 8080 443 3389)
 for port in "${blocked_ports[@]}"; do
     iptables -A OUTPUT -m owner --uid-owner 100000-165536 -d 0.0.0.0/0 -p tcp --dport "$port" -j DROP
     iptables -A OUTPUT -m owner --uid-owner 100000-165536 -d 0.0.0.0/0 -p udp --dport "$port" -j DROP
 done
+
+# 屏蔽仓库访问
