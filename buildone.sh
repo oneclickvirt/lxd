fbdd9d2c78fe19289b9f8692eba6587de873c1dc
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-16 09:39:27 +0800

    Update buildone.sh

--- buildone.sh
+++ buildone.sh
@@ -51,6 +51,9 @@ lxc exec "$1" -- curl -L https://raw.githubusercontent.com/spiritLHLS/lxc/main/s
 lxc exec "$1" -- chmod 777 ssh.sh
 lxc exec "$1" -- dos2unix ssh.sh
 lxc exec "$1" -- sudo ./ssh.sh $passwd
+lxc exec "$1" -- curl -L https://github.com/spiritLHLS/lxc/raw/main/config.sh -o config.sh 
+lxc exec "$1" -- chmod +x config.sh
+lxc exec "$1" -- bash config.sh
 lxc config device add "$1" ssh-port proxy listen=tcp:0.0.0.0:$sshn connect=tcp:127.0.0.1:22
 lxc config device add "$1" nattcp-ports proxy listen=tcp:0.0.0.0:$nat1-$nat2 connect=tcp:127.0.0.1:$nat1-$nat2
 lxc config device add "$1" natudp-ports proxy listen=udp:0.0.0.0:$nat1-$nat2 connect=udp:127.0.0.1:$nat1-$nat2
