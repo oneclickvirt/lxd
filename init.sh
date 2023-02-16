4f37669f03377cc8eaef788ac632e6bb5063c634
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-16 10:18:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 # by https://github.com/spiritLHLS/lxc
 # cd /root
 # ./init.sh NAT服务器前缀 数量
-# 2023.01.15
+# 2023.02.16
 
 rm -rf log
 lxc init images:debian/10 "$1" -c limits.cpu=1 -c limits.memory=256MiB
@@ -31,6 +31,8 @@ lxc config set "$1" security.nesting true
 #   lxc config set "$1" security.syscalls.intercept.mknod true
 #   lxc config set "$1" security.syscalls.intercept.setxattr true
 # fi
+# 屏蔽端口
+blocked_ports=( 20 21 22 23 25 53 67 68 69 110 139 143 161 389 443 1433 1521 2094 3306 3389 5000 5432 5632 5900 6379 7001 8888 9200 10000 27017 22122 54321 65432 )
 # 批量创建容器
 for ((a=1;a<="$2";a++)); do
   lxc copy "$1" "$1"$a
@@ -54,6 +56,12 @@ for ((a=1;a<="$2";a++)); do
   lxc exec "$1"$a -- curl -L https://github.com/spiritLHLS/lxc/raw/main/config.sh -o config.sh 
   lxc exec "$1"$a -- chmod +x config.sh
   lxc exec "$1"$a -- bash config.sh
+  container_name="$1"$a 
+  container_ip_address=$(lxc list ${container_name} -c 4 | awk 'NR==4 {print $2}')
+  for port in "${blocked_ports[@]}"; do
+    iptables -A OUTPUT -s ${container_ip_address} -d 0.0.0.0/0 -p tcp --dport ${port} -j DROP
+    iptables -A OUTPUT -s ${container_ip_address} -d 0.0.0.0/0 -p udp --dport ${port} -j DROP
+  done
   lxc config device add "$1"$a ssh-port proxy listen=tcp:0.0.0.0:$sshn connect=tcp:127.0.0.1:22
   lxc config device add "$1"$a nattcp-ports proxy listen=tcp:0.0.0.0:$nat1-$nat2 connect=tcp:127.0.0.1:$nat1-$nat2
   lxc config device add "$1"$a natudp-ports proxy listen=udp:0.0.0.0:$nat1-$nat2 connect=udp:127.0.0.1:$nat1-$nat2
