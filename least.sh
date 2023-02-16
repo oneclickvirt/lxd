bc49ae4638a20fb88263fdd904ea75691d920881
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-02-16 12:41:05 +0800

    Update least.sh

--- least.sh
+++ least.sh
@@ -26,6 +26,9 @@ lxc config set "$1" security.nesting true
 # fi
 # 屏蔽端口
 blocked_ports=( 20 21 22 23 25 53 67 68 69 110 139 143 161 389 443 1433 1521 2094 3306 3389 5000 5432 5632 5900 6379 7001 8888 9200 10000 27017 22122 54321 65432 )
+for port in "${blocked_ports[@]}"; do
+  iptables -I FORWARD -o eth0 -p tcp --dport ${port} -j DROP
+done
 # 批量创建容器
 for ((a=1;a<="$2";a++)); do
   lxc copy "$1" "$1"$a
@@ -46,15 +49,6 @@ for ((a=1;a<="$2";a++)); do
   lxc exec "$1"$a -- curl -L https://github.com/spiritLHLS/lxc/raw/main/config.sh -o config.sh 
   lxc exec "$1"$a -- chmod +x config.sh
   lxc exec "$1"$a -- bash config.sh
-#   container_name="$1"$a
-#   container_ip_address=$(lxc list ${container_name} -c 4 | awk 'NR==4 {print $2}')
-  for port in "${blocked_ports[@]}"; do
-    iptables -I FORWARD -o eth0 -p tcp --dport ${port} -j DROP
-#     iptables -A OUTPUT -s ${container_ip_address} -d 0.0.0.0/0 -p tcp --dport ${port} -j DROP
-#     iptables -A OUTPUT -s ${container_ip_address} -d 0.0.0.0/0 -p udp --dport ${port} -j DROP
-#     iptables -A INPUT -d ${container_ip_address} -s 0.0.0.0/0 -p tcp --sport ${port} -j DROP
-#     iptables -A INPUT -d ${container_ip_address} -s 0.0.0.0/0 -p udp --sport ${port} -j DROP
-  done
   lxc config device add "$1"$a ssh-port proxy listen=tcp:0.0.0.0:$sshn connect=tcp:127.0.0.1:22
   echo "$name $sshn $passwd" >> log
 done
