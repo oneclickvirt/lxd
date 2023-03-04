7e16b340071c1ae95a84926179e0a4090a1d2f84
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-03-04 14:42:13 +0800

    Update buildone.sh

--- buildone.sh
+++ buildone.sh
@@ -6,6 +6,8 @@
 # cd /root
 # 输入
 # ./buildone.sh 服务器名称 内存大小 硬盘大小 SSH端口 外网起端口 外网止端口 下载速度 上传速度 是否启用IPV6(Y or N)
+# 如果 外网起端口 外网止端口 都设置为0则不做区间外网端口映射了，只映射基础的SSH端口
+
 # 创建容器
 name="${1:-test}"
 memory="${2:-256}"
@@ -65,18 +67,25 @@ lxc exec "$name" -- chmod +x config.sh
 lxc exec "$name" -- bash config.sh
 lxc exec "$name" -- history -c
 lxc config device add "$name" ssh-port proxy listen=tcp:0.0.0.0:$sshn connect=tcp:127.0.0.1:22
-lxc config device add "$name" nattcp-ports proxy listen=tcp:0.0.0.0:$nat1-$nat2 connect=tcp:127.0.0.1:$nat1-$nat2
-lxc config device add "$name" natudp-ports proxy listen=udp:0.0.0.0:$nat1-$nat2 connect=udp:127.0.0.1:$nat1-$nat2
-# 生成的小鸡信息写入log并打印
-echo "$name $sshn $passwd $nat1 $nat2" >> "$name"
-echo "$name $sshn $passwd $nat1 $nat2"
 # 是否要创建V6地址
 if [ -n "$9" ]; then
   if [ "$9" == "Y" ]; then
     if [ ! -f "./build_ipv6_network.sh" ]; then
       # 如果不存在，则从指定 URL 下载并添加可执行权限
-      curl -L https://raw.githubusercontent.com/spiritLHLS/lxc/main/build_ipv6_network.sh -o build_ipv6_network.sh && chmod +x build_ipv6_network.sh
+      curl -L https://raw.githubusercontent.com/spiritLHLS/lxc/main/build_ipv6_network.sh -o build_ipv6_network.sh && chmod +x build_ipv6_network.sh > /dev/null 2>&1
     fi
-    ./build_ipv6_network.sh "$name"
+    ./build_ipv6_network.sh "$name" > /dev/null 2>&1
   fi
+fi
+if [ "$nat1" != "0" ] && [ "$nat2" != "0" ]; then
+  lxc config device add "$name" nattcp-ports proxy listen=tcp:0.0.0.0:$nat1-$nat2 connect=tcp:127.0.0.1:$nat1-$nat2
+  lxc config device add "$name" natudp-ports proxy listen=udp:0.0.0.0:$nat1-$nat2 connect=udp:127.0.0.1:$nat1-$nat2
+  # 生成的小鸡信息写入log并打印
+  echo "$name $sshn $passwd $nat1 $nat2" >> "$name"
+  echo "$name $sshn $passwd $nat1 $nat2"
+  exit 1
+fi
+if [ "$nat1" == "0" ] && [ "$nat2" == "0" ]; then
+  echo "$name $sshn $passwd" >> "$name"
+  echo "$name $sshn $passwd" 
 fi
