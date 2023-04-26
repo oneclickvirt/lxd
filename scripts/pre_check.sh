7d1f749afb590fda7d835da77681c96ce9cf0885
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-26 11:32:36 +0800

    Update pre_check.sh

--- scripts/pre_check.sh
+++ scripts/pre_check.sh
@@ -3,8 +3,21 @@
 # 2023.04.26
 # 预检测本机是否符合开设小鸡的要求
 
-# 必须以root运行脚本
-[[ $(id -u) != 0 ]] && echo "必须以root身份执行脚本，请切换到root权限再使用本套脚本" && exit 1
+_red() { echo -e "\033[31m\033[01m$@\033[0m"; }
+_green() { echo -e "\033[32m\033[01m$@\033[0m"; }
+_yellow() { echo -e "\033[33m\033[01m$@\033[0m"; }
+_blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
+
+# 必须以root身份运行脚本，且脚本必须在/root目录下
+if [[ $(id -u) != 0 ]]; then
+    _yellow "必须以root身份运行脚本，请切换到root权限再使用本套脚本。"
+    exit 1
+elif [[ $(pwd) != /root* ]]; then
+    _yellow "脚本必须在/root目录下，请动到/root目录下再运行脚本。"
+    exit 1
+else
+    _green "本机路径符合要求"
+fi
 
 # 必须是全虚拟化的架构
 virtcheck=$(systemd-detect-virt)
@@ -14,8 +27,10 @@ case "$virtcheck" in
   * ) VIRT='kvm';;
 esac
 if [ $VIRT = "openvz" ]; then
-  echo "OVZ架构无法使用本套脚本，请使用别的虚拟化的服务器做母鸡，如KVM"
+  _yellow "OVZ架构无法使用本套脚本，请使用别的虚拟化的服务器做母鸡，如KVM"
   exit 1
+else
+  _green "本机架构符合要求"
 fi
 
 # 检查系统版本是否符合要求
@@ -28,34 +43,44 @@ else
 fi
 
 if [[ $distro != "Ubuntu" && ($distro != "Debian" || $codename -lt "jessie") ]]; then
-    echo "系统版本不符合要求，需要Ubuntu或Debian 8+。"
+    _yellow "本机系统不符合要求，需要 Ubuntu 或 Debian 8+"
     exit 1
+else
+    _green "本机系统符合要求"
 fi
 
 # 检查内存大小是否符合要求
 mem=$(awk '/MemTotal/{print $2}' /proc/meminfo)
 if [ $mem -lt 524288 ]; then
-    echo "内存大小不符合要求，需要至少512MB。"
+    _yellow "内存大小不符合要求，需要至少512MB。"
     exit 1
+else
+    _green "本机内存符合要求"
 fi
 
 # 检查磁盘空间是否符合要求
 disk=$(df / | awk '/\//{print $4}')
 if [ $disk -lt 10485760 ]; then
-    echo "磁盘空间不符合要求，需要至少10G。"
+    _yellow "本机硬盘空间不符合要求，需要至少10G。"
     exit 1
+else
+    _green "本机硬盘符合要求"
 fi
 
 # 检查网络是否符合要求
 if ! ping -c 1 -w 6 raw.githubusercontent.com >/dev/null 2>&1; then
-    echo "网络无法连接Github的raw页面，请检查网络连接。"
+    _yellow "本机网络无法连接Github的raw页面，请检查网络连接"
     exit 1
+else
+    _green "本机网络连通性符合要求"
 fi
 
 ip=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut -f1 -d'/')
 if [ -z "$ip" ]; then
-    echo "无法找到独立的IPv4地址，请检查网络连接。"
+    _yellow "本机无法找到独立的IPv4地址，请检查网络连接"
     exit 1
+else
+  _green "本机IP符合要求"
 fi
 
-echo "本机符合作为母鸡的要求。"
+_green "本机符合作为母鸡的要求"
