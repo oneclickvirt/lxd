9d9def5f46f0425e5e6fb13d4dea0b974c3612cf
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-04-27 11:12:40 +0800

    Update build_ipv6_network.sh

--- scripts/build_ipv6_network.sh
+++ scripts/build_ipv6_network.sh
@@ -11,6 +11,15 @@ _red() { echo -e "\033[31m\033[01m$@\033[0m"; }
 _green() { echo -e "\033[32m\033[01m$@\033[0m"; }
 _yellow() { echo -e "\033[33m\033[01m$@\033[0m"; }
 _blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "utf8|UTF-8")
+if [[ -z "$utf8_locale" ]]; then
+  _yellow "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  _green "Locale set to $utf8_locale"
+fi
 
 # 检查所需模块是否存在，如果不存在则安装
 install_required_modules() {
