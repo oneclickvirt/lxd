c7825bc14395f78fdd153e27d58f65cb2b940eaa
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-06-14 14:34:05 +0800

    Create check-ipv6.sh

---
+++ scripts/check-ipv6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# by https://github.com/spiritLHLS/lxc
+
+file="/etc/iptables/rules.v6"
+array=()
+
+while IFS= read -r line; do
+    if [[ $line == "-A PREROUTING -d"* ]]; then
+        parameter="${line#*-d }"
+        parameter="${parameter%%/*}"
+        array+=("$parameter")
+    fi
+done < "$file"
+
+interface=$(lshw -C network | awk '/logical name:/{print $3}' | head -1)
+for parameter in "${array[@]}"; do
+    ip addr add "$parameter"/64 dev "$interface"
+done
