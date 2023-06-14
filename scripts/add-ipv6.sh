e4002f09895fd398490d3d78df835a98dcec3a43
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-06-14 14:42:02 +0800

    Update add-ipv6.sh

--- scripts/add-ipv6.sh
+++ scripts/add-ipv6.sh
@@ -12,7 +12,11 @@ while IFS= read -r line; do
     fi
 done < "$file"
 
-interface=$(lshw -C network | awk '/logical name:/{print $3}' | head -1)
-for parameter in "${array[@]}"; do
-    ip addr add "$parameter"/64 dev "$interface"
-done
+if [ ${#array[@]} -eq 0 ]; then
+    echo "Empty IPV6 array"
+else
+    interface=$(lshw -C network | awk '/logical name:/{print $3}' | head -1)
+    for parameter in "${array[@]}"; do
+        ip addr add "$parameter"/64 dev "$interface"
+    done
+fi
