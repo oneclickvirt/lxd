5b8aa28c352921700182748e202e3e363a4bf6df
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-06-20 17:36:01 +0800

    Create alpinessh.sh

---
+++ scripts/alpinessh.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#https://github.com/spiritLHLS/lxc
+
+if [ "$(cat /etc/os-release | grep -E '^ID=' | cut -d '=' -f 2)" != "alpine" ]
+then
+  echo "This script only supports Alpine Linux."
+  exit 1
+fi
+if [ "$(id -u)" -ne 0 ]
+then
+  echo "This script must be executed with root privileges."
+  exit 1
+fi
+apk update
+apk add --no-cache wget curl openssh-server sshpass
+cd /etc/ssh
+ssh-keygen -A
+sshport=22
+sed -i.bak '/^#PermitRootLogin/c PermitRootLogin yes' /etc/ssh/sshd_config
+sed -i.bak '/^#PasswordAuthentication/c PasswordAuthentication yes' /etc/ssh/sshd_config
+sed -i.bak '/^#ListenAddress/c ListenAddress 0.0.0.0' /etc/ssh/sshd_config
+sed -i.bak '/^#AddressFamily/c AddressFamily any' /etc/ssh/sshd_config
+sed -i.bak "s/^#\?Port.*/Port $sshport/" /etc/ssh/sshd_config
+/usr/sbin/sshd
+echo root:"$1" | chpasswd root
+rm -f "$0"
