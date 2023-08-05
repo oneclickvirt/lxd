cdab799e36c44c4696acce843eeef29f973e7f5d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-05 09:16:45 +0800

    Update alpinessh.sh

--- scripts/alpinessh.sh
+++ scripts/alpinessh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # by https://github.com/spiritLHLS/lxc
-# 2023.07.24
+# 2023.08.05
 
 if [ "$(cat /etc/os-release | grep -E '^ID=' | cut -d '=' -f 2)" != "alpine" ]
 then
@@ -19,12 +19,16 @@ if [ -f "/etc/motd" ]; then
 fi
 cd /etc/ssh
 ssh-keygen -A
-sed -i.bak '/^#PermitRootLogin\|PermitRootLogin/c PermitRootLogin yes' /etc/ssh/sshd_config
-sed -i.bak "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sed -i.bak '/^#ListenAddress\|ListenAddress/c ListenAddress 0.0.0.0' /etc/ssh/sshd_config
-sed -i.bak '/^#AddressFamily\|AddressFamily/c AddressFamily any' /etc/ssh/sshd_config
-sed -i.bak "s/^#\?\(Port\).*/\1 22/" /etc/ssh/sshd_config
-sed -i.bak -E 's/^#?(Port).*/\1 22/' /etc/ssh/sshd_config
+sed -i '/^#PermitRootLogin\|PermitRootLogin/c PermitRootLogin yes' /etc/ssh/sshd_config
+sed -i "s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sed -i '/^#ListenAddress\|ListenAddress/c ListenAddress 0.0.0.0' /etc/ssh/sshd_config
+sed -i '/^#AddressFamily\|AddressFamily/c AddressFamily any' /etc/ssh/sshd_config
+sed -i "s/^#\?\(Port\).*/\1 22/" /etc/ssh/sshd_config
+sed -i -E 's/^#?(Port).*/\1 22/' /etc/ssh/sshd_config
+sed -E -i 's/preserve_hostname:[[:space:]]*false/preserve_hostname: true/g' /etc/cloud/cloud.cfg
+sed -E -i 's/disable_root:[[:space:]]*true/disable_root: false/g' /etc/cloud/cloud.cfg
+sed -E -i 's/ssh_pwauth:[[:space:]]*false/ssh_pwauth:   true/g' /etc/cloud/cloud.cfg
 /usr/sbin/sshd
+rc-update add sshd default
 echo root:"$1" | chpasswd root
 rm -f "$0"
