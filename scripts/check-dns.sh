e84fbcea2130ad6b247f05166384dd73bbf8d09d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-09-02 20:42:20 +0800

    Update check-dns.sh

--- scripts/check-dns.sh
+++ scripts/check-dns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #from https://github.com/spiritLHLS/lxd
-# 2023.06.29
+# 2023.09.02
 
 DNS_SERVER="2001:4860:4860::8844"
 RESOLV_CONF="/etc/resolv.conf"
@@ -10,8 +10,6 @@ grep -q "^nameserver ${DNS_SERVER}$" ${RESOLV_CONF}
 if [ $? -eq 0 ]; then
     echo "DNS server ${DNS_SERVER} already exists in ${RESOLV_CONF}."
 else
-    chattr -i /etc/resolv.conf
     echo "Adding DNS server ${DNS_SERVER} to ${RESOLV_CONF}..."
     echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4\nnameserver 2606:4700:4700::1111\nnameserver 2001:4860:4860::8888\nnameserver 2001:4860:4860::8844" >>${RESOLV_CONF}
-    chattr +i /etc/resolv.conf
 fi
