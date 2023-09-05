3ddc90c4869471b621ffeb95ad02e631f9fcaf3a
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-09-05 10:47:18 +0800

    Update panel_init.sh

--- panel_scripts/panel_init.sh
+++ panel_scripts/panel_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # by https://github.com/spiritLHLS/lxd
-# 2023.09.02
+# 2023.09.05
 
 # curl -L https://raw.githubusercontent.com/spiritLHLS/lxd/main/scripts/lxdinstall.sh -o lxdinstall.sh && chmod +x lxdinstall.sh && bash lxdinstall.sh
 
@@ -388,36 +388,44 @@ if [ -f "/etc/systemd/logind.conf" ]; then
 fi
 # 环境安装
 # 安装vnstat
-apt update
-apt install wget sudo curl -y
-apt install linux-headers-$(uname -r) -y
-wget https://github.com/vergoh/vnstat/releases/download/v2.10/vnstat-2.10.tar.gz
-# gd gd-devel
-apt install build-essential libsqlite3-dev -y
-tar -xvf vnstat-2.10.tar.gz
-cd vnstat-2.10/
-sudo ./configure --prefix=/usr --sysconfdir=/etc
-sudo make
-sudo make install
-cp -v examples/systemd/vnstat.service /etc/systemd/system/
-systemctl enable vnstat
-systemctl start vnstat
-cp -v examples/init.d/redhat/vnstat /etc/init.d/
-sudo sed -i '/deb http:\/\/archive.ubuntu.com\/ubuntu\/ trusty main universe restricted multiverse/d' /etc/apt/sources.list
-grep -q "deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted multiverse" /etc/apt/sources.list || echo "deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted multiverse" >>/etc/apt/sources.list
-apt install chkconfig -y
-if [ $? -ne 0 ]; then
-    apt install sysv-rc-conf -y
-    if [ $? -ne 0 ]; then
-        apt update && apt install sysv-rc-conf -y
-    fi
-fi
-! chkconfig vnstat on && echo "replace chkconfig with sysv-rc-conf" && sysv-rc-conf vnstat on
-service vnstat start
-# apt install vnstati -y
+
+# apt update
+# apt install wget sudo curl -y
+# apt install linux-headers-$(uname -r) -y
+# wget https://github.com/vergoh/vnstat/releases/download/v2.10/vnstat-2.10.tar.gz
+# # gd gd-devel
+# apt install build-essential libsqlite3-dev -y
+# tar -xvf vnstat-2.10.tar.gz
+# cd vnstat-2.10/
+# sudo ./configure --prefix=/usr --sysconfdir=/etc
+# sudo make
+# sudo make install
+# cp -v examples/systemd/vnstat.service /etc/systemd/system/
+# systemctl enable vnstat
+# systemctl start vnstat
+# cp -v examples/init.d/redhat/vnstat /etc/init.d/
+# sudo sed -i '/deb http:\/\/archive.ubuntu.com\/ubuntu\/ trusty main universe restricted multiverse/d' /etc/apt/sources.list
+# grep -q "deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted multiverse" /etc/apt/sources.list || echo "deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted multiverse" >>/etc/apt/sources.list
+# apt install chkconfig -y
+# if [ $? -ne 0 ]; then
+#     apt install sysv-rc-conf -y
+#     if [ $? -ne 0 ]; then
+#         apt update && apt install sysv-rc-conf -y
+#     fi
+# fi
+# ! chkconfig vnstat on && echo "replace chkconfig with sysv-rc-conf" && sysv-rc-conf vnstat on
+# service vnstat start
+# # apt install vnstati -y
+
+systemctl enable snapd
+systemctl start snapd
+snap install vnstat
+export PATH="$PATH:/snap/bin"
+
 vnstat -v
 vnstatd -v
 ! vnstati -v && echo "vnstat 编译安装无vnstati工具，如需使用请使用命令 apt install vnstati -y 覆盖安装apt源版本"
+
 # 加装证书
 wget ${cdn_success_url}https://raw.githubusercontent.com/spiritLHLS/lxd/main/panel_scripts/client.crt -O /root/snap/lxd/common/config/client.crt
 chmod 777 /root/snap/lxd/common/config/client.crt
