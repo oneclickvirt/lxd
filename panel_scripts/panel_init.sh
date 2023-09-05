2fb07aa1a2f15c68d7cd176b7a02e87842e5b512
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-09-05 20:15:55 +0800

    Update panel_init.sh

--- panel_scripts/panel_init.sh
+++ panel_scripts/panel_init.sh
@@ -388,44 +388,26 @@ if [ -f "/etc/systemd/logind.conf" ]; then
 fi
 # 环境安装
 # 安装vnstat
-
-# apt update
-# apt install wget sudo curl -y
-# apt install linux-headers-$(uname -r) -y
-# wget https://github.com/vergoh/vnstat/releases/download/v2.10/vnstat-2.10.tar.gz
-# # gd gd-devel
-# apt install build-essential libsqlite3-dev -y
-# tar -xvf vnstat-2.10.tar.gz
-# cd vnstat-2.10/
-# sudo ./configure --prefix=/usr --sysconfdir=/etc
-# sudo make
-# sudo make install
-# cp -v examples/systemd/vnstat.service /etc/systemd/system/
-# systemctl enable vnstat
-# systemctl start vnstat
-# cp -v examples/init.d/redhat/vnstat /etc/init.d/
-# sudo sed -i '/deb http:\/\/archive.ubuntu.com\/ubuntu\/ trusty main universe restricted multiverse/d' /etc/apt/sources.list
-# grep -q "deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted multiverse" /etc/apt/sources.list || echo "deb http://archive.ubuntu.com/ubuntu/ trusty main universe restricted multiverse" >>/etc/apt/sources.list
-# apt install chkconfig -y
-# if [ $? -ne 0 ]; then
-#     apt install sysv-rc-conf -y
-#     if [ $? -ne 0 ]; then
-#         apt update && apt install sysv-rc-conf -y
-#     fi
-# fi
-# ! chkconfig vnstat on && echo "replace chkconfig with sysv-rc-conf" && sysv-rc-conf vnstat on
-# service vnstat start
-# # apt install vnstati -y
-
-systemctl enable snapd
-systemctl start snapd
-apt install vnstati -y
-snap install vnstat
-export PATH="$PATH:/snap/bin"
-
+install_package make
+install_package gcc
+install_package libc6-dev
+install_package libsqlite3-0
+install_package libsqlite3-dev
+install_package libgd3 
+install_package libgd-dev
+cd /usr/src
+wget https://humdi.net/vnstat/vnstat-2.11.tar.gz
+chmod 777 vnstat-2.11.tar.gz
+tar zxvf vnstat-2.11.tar.gz
+cd vnstat-2.11
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+cp -v examples/systemd/vnstat.service /etc/systemd/system/
+systemctl enable vnstat
+systemctl start vnstat
+pgrep -c vnstatd
 vnstat -v
 vnstatd -v
-! vnstati -v && echo "vnstat 编译安装无vnstati工具，如需使用请使用命令 apt install vnstati -y 覆盖安装apt源版本"
+vnstati -v
 
 # 加装证书
 wget ${cdn_success_url}https://raw.githubusercontent.com/spiritLHLS/lxd/main/panel_scripts/client.crt -O /root/snap/lxd/common/config/client.crt
